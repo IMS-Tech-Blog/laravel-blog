d720f8d4f032ff6338c262a9c3599ddfed7ab80a
zhancheng.li <zhancheng.li@zhanchenglideMacBook-Pro.local>
2016-12-06 23:30:08 +0800

    add css-module to escape the same name of writing css...

--- resources/statics/conf/loaders.config.js
+++ resources/statics/conf/loaders.config.js
@@ -10,21 +10,21 @@ export const babel = { test: /\.js$/, /*loader: 'babel', */use: ['react-hot', 'b
 // eslint-loader
 export const eslint = { test: /\.js$/, loader: 'eslint', /*exclude: /node_modules/, */enforce: 'pre', /*options: { configFile: path.join(ROOT_PATH, '.eslintrc') }*/ };
 // css-loader
-export const css = { test: /\.css$/, loaders: ['style', 'css'] };
+export const css = { test: /\.css$/, use: ['style', 'css'] };
 // url-loader
 export const url = { test: /\.(jpg|png|gif)$/, loader: 'url?limit=8192' };
 // file-loader
 export const file = { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' };
 // sass-loader
-export const sass = { test: /\.scss$/, loaders: ['style', 'css', 'sass', 'postcss'] };
+export const sass = { test: /\.scss$/, use: ['style', 'css?modules', 'sass'/*, 'postcss'*/] };
 // sass-lint-loader
-export const sasslint = { test: /\.scss$/, loader: 'scsslint', enforce: 'pre' };
+export const sasslint = { test: /\.scss$/, loader: 'sasslint', enforce: 'pre' };
 // html-minify-loader
 export const htmlmin = { test: /\.html$/, loader: 'html-minify' };
 
 export const allLoaders = [
   babel,
-  eslint,
+  // eslint,
   css,
   url,
   file,
--- resources/statics/src/frontend/components/App.js
+++ resources/statics/src/frontend/components/App.js
@@ -1,48 +1,11 @@
 import React from 'react';
+import style from './App.scss';
 
 const App = () => (
-  <div>
-    <h2>Hello,nnnnnn</h2>
-    <h5>aaaa</h5>
+  <div className={style.root}>
+    <aside className={style.side}>side</aside>
+    <main className={style.main}>main</main>
   </div>
 );
 
-class AppClass extends React.Component {
-	constructor() {
-		super();
-	}
-
-	state = {
-		isShow: false
-	}
-
-	componentWillMount() {
-		console.log('This is the period');
-	}
-
-	showH5 = () => {
-		this.setState({
-			isShow: true
-		});
-	}
-	hideH5 = () => {
-		this.setState({
-			isShow: false
-		});
-
-	}
-
-	render() {
-		return (
-			<div>
-			<h2>Hello fantasty aaaa</h2>
-			<h5 style={{ display: this.state.isShow ? 'block' : 'none' }}>I am react</h5>
-			<button onClick={this.showH5}>show</button>
-			<button onClick={this.hideH5}>hide</button>
-			</div>
-		);
-	}
-}
-
-
-export default AppClass;
+export default App;
