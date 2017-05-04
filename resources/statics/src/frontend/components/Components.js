fd6ef9d414568531f0f972d1db105d371e436000
zhancheng.li <zhancheng.li@zhanchenglideMacBook-Pro.local>
2017-05-04 16:10:12 +0800

    delete unused files

--- resources/statics/src/frontend/components/Components.js
+++
@@ -1,5 +0,0 @@
-/**
- * All Components from the separated files
- */
-export Intro      from './Intro/Intro.js';
-export MagicImage from './MagicImage/MagicImage.js'
--- resources/statics/src/frontend/redux/constants/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * constants in redux
- */
-import keyMirror from 'fbjs/lib/keyMirror';
-
-export default keyMirror({
-  ENTER_EFFECT: null,
-  LEAVE_EFFECT: null
-});
